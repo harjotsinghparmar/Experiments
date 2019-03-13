dfe35e4ae82fb8747f2759ee558453c82a266f5b
Harjot Singh Parmar <harjot.s.parmar@gmail.com>
2019-03-12 22:21:54 -0400

    added new inputs and changed some settings

--- Design /Font Experiments/d3_sliders.js
+++ Design /Font Experiments/d3_sliders.js
@@ -48,3 +48,61 @@ var gSimple = d3
 
 gSimple.call(lineHsimple);
 
+
+
+// change line width of the para using slider
+var content_width = [0,20,40,60,80,100];
+
+var contentWsimple = d3
+  .sliderBottom()
+  .min(d3.min(content_width))
+  .max(d3.max(content_width))
+  .width(200)
+  .tickFormat(d3.format('.3'))
+  .ticks(5)
+  .step(20)
+  .default(1)
+  .on('onchange', val => {
+    d3.selectAll('.content').style('max-width',val+'%');
+  });
+
+var gSimple = d3
+  .select('div#linew-simple')
+  .append('svg')
+  .attr('width', 400)
+  .attr('height', 100)
+  .append('g')
+  .attr('transform', 'translate(35,30)');
+
+gSimple.call(contentWsimple);
+
+
+
+// change header font size width of the para using slider
+var fsize = [12,16,18,24,36];
+
+var fsizesimple = d3
+  .sliderBottom()
+  .min(d3.min(fsize))
+  .max(d3.max(fsize))
+  .width(200)
+  .tickFormat(d3.format('.3'))
+  .ticks(5)
+  .step(4)
+  .default(1)
+  .on('onchange', val => {
+    d3.selectAll('.pheader').style('font-size',val+'px');
+  });
+
+var gSimple = d3
+  .select('div#fsize')
+  .append('svg')
+  .attr('width', 400)
+  .attr('height', 100)
+  .append('g')
+  .attr('transform', 'translate(35,30)');
+
+gSimple.call(fsizesimple);
+
+
+
